af463bae00c39f656f81f4677246c522de79ebb6
Jiwon <js6450@nyu.edu>
2021-03-16 19:05:25 -0400

    update sketch.js

--- public/sketch.js
+++ public/sketch.js
@@ -1,10 +1,27 @@
 let socket = io();
 
+let nextTrain;
+let nextTrainTime;
+
+let lineOne;
+
 function setup(){
-    socket.emit('requestData', 237)
+    socket.emit('requestData', 636);
     socket.on('data', getData);
 }
 
 function getData(data){
     console.log(data);
+
+    lineOne = data.lines[0];
+
+    nextTrain = data.lines[0].departures.S[0];
+    nextTrainTime = new Date(nextTrain.time * 1000);
+
+    let currentTime = new Date();
+
+    console.log(nextTrainTime);
+
+    console.log(nextTrainTime.getHours());
+    console.log(nextTrainTime.getMinutes());
 }
